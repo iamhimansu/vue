83106a01c57d027fa549777dadc5bcf4acaf6c39
Himanshu Raj Aman <himanshu.aman@samarth.ac.in>
2023-09-01 17:52:56 +0530

    Added deploy

---
+++ src/components/ArticleSearchBox.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+name: "ArticleSearchBox"
+}
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ContentPadded.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+name: "ContentPadder"
+}
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped>
+
+</style>
